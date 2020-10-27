f5d9ecb3af59fad9fef808c0f223430d46e33f17
holoIA <xzlukeholo@gmail.com>
2020-10-27 13:05:30 +0800

    Added function to calculate average cost

--- products.py
+++ products.py
@@ -6,10 +6,22 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入花費金額:')
-	p = [name, price + '元']
+	p = [name, price]
 	products.append(p)
 
-print(products)
+print('----------------------------------------')
+
+sum_p = 0
+num_p = 0
+for p in products:
+	print(p[0], '的花費是:', p[1] + '元')
+	p[1] = int(p[1])
+	sum_p += p[1]
+	num_p += 1
+
+average = sum_p / num_p
+average = str(average)
+print('平均花費是:', average + '元')
 
 # print('第一個花費名稱是:', products[0][0])
 # print('第一個花費金額是:', products[0][1])
