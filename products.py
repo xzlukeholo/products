eb04fc556ddec326f366199d991528fb58296c6d
holoIA <xzlukeholo@gmail.com>
2020-10-27 23:38:06 +0800

    Add the function of saving as a file and add a header

--- products.py
+++ products.py
@@ -2,13 +2,14 @@ products = []
 
 print('輸入完畢請輸入q')
 while True:
-	name = input('請輸入花費名稱:')
+	name = input('請輸入開銷名稱:')
 	if name == 'q':
 		break
 	price = input('請輸入花費金額:')
 	p = [name, price]
 	products.append(p)
-
+# print('第一個花費名稱是:', products[0][0])
+# print('第一個花費金額是:', products[0][1])
 print('----------------------------------------')
 
 sum_p = 0
@@ -23,5 +24,9 @@ average = sum_p / num_p
 average = str(average)
 print('平均花費是:', average + '元')
 
-# print('第一個花費名稱是:', products[0][0])
-# print('第一個花費金額是:', products[0][1])
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('項目,金額\n' )
+	for p in products:
+		p[1] = str(p[1])
+		f.write(p[0] + ',' + p[1] + '元' + '\n')
+
